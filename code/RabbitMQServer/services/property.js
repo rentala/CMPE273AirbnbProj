6b95c17ed3428c94ab97c4de129ecd16b166e6f3
Smitha Muthuvenkataramani <smitha.muthuvenkataramani@sjsu.edu>
2016-11-16 21:15:58 -0800

    adding searchProperty API functionality.

--- code/RabbitMQServer/services/property.js
+++ code/RabbitMQServer/services/property.js
@@ -2,8 +2,8 @@
  * Created by Rentala on 15-11-2016.
  */
 var tool = require("../utili/common");
-
-
+var sql_queries = require('../db/sql_queries');
+var mysql = require('../db/mysql');
 var listProperty = {
     handle_request : function (connection, msg, callback){
         try{
@@ -35,6 +35,7 @@ var listProperty = {
 var searchProperty = {
     handle_request : function (connection,msg,callback) {
         var res = {};
+        var available_property = [];
         try{
             var coll = connection.mongoConn.collection('property');
             coll.find({city: msg.city,state:msg.state,zipcode:msg.zipcode,category:msg.category,start_date:{$lte:new Date(msg.start_date)},end_date:{$gte:new Date(msg.end_date)}},function(err, records){
@@ -45,10 +46,24 @@ var searchProperty = {
                 }
                 records.toArray(function (e,recs) {
                     console.log(recs);
-                    if (recs) {
-                        res.code = "200";
-                        res.value = recs;
-                        callback(null, res);
+                    if (recs){
+                        available_property = getPropertyArray(recs);
+                        mysql.fetchTripDates(function (err, result) {
+                            if(err){
+                                throw err;
+                            }
+                            else {
+                                if(result.length>0){
+                                    res.code = "200";
+                                    res.value = result;
+                                    callback(null, res);
+                                }
+                                else {
+                                    res.code = "400";
+                                    callback(null, res);
+                                }
+                            }
+                        },sql_queries.FETCH_TRIP_DATES,[available_property]);
                     }
                     else {
                         res.code = "400";
@@ -66,6 +81,15 @@ var searchProperty = {
         }
     }
 };
+
+function  getPropertyArray(records) {
+    var propArray = [];
+    for(var i=0;i<records.length;i++){
+        propArray.push(records[i]._id);
+    }
+    console.log(propArray);
+    return propArray;
+}
 
 
 exports.searchProperty = searchProperty;
