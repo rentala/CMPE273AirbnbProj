8822531d1323bb852c86b81964b5ce2d44140500
varshak <varsha-kankariya@users.noreply.github.com>
2016-11-18 15:23:22 -0800

    Updated for github testing 2 : devloper 2

--- code/AirbnbClient/routes/admin.js
+++ code/AirbnbClient/routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mq_client = require('../rpc/client');
 var passport = require('passport');
 
-//POST method to approve a host for listing properties on Airbnb....Smitha
+//POST method to approve a host for listing properties on Airbnb....testing 2
 router.post('/approveHost', function (req, res)  {
     
 	console.log("Request Data  : " + JSON.stringify(req.body));
