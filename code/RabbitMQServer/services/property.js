eaa6028920c6098a7e09b22e6c9cb3ed0ee1842c
jpranjal <jpranjal1304@gmail.com>
2016-11-19 02:35:33 +0800

    code to add property into MYSQL bidding table while insertion. Pleaes dont remove it again!

--- code/RabbitMQServer/services/property.js
+++ code/RabbitMQServer/services/property.js
@@ -6,31 +6,50 @@ var sql_queries = require('../db/sql_queries');
 var mysql = require('../db/mysql');
 var ObjectID = require('mongodb').ObjectID;
 var listProperty = {
-    handle_request : function (connection, msg, callback){
-        try{
-            var res = {};
-            var coll = connection.mongoConn.collection('property');
-            coll.insert(msg, function(err, prop){
-                if(err){
-                    tool.logError(err);
-                    res.code ="400";
-                    callback(null, res);
-                }
-                else
-                {
-                    res.user_id = prop.insertedIds,
-                    res.code ="200";
-                    callback(null, res);
-                }
-            });
-        }
-        catch(err){
-            tool.logError(err);
-            res.statusCode = "500";
-            callback(null, res);
-        }
-    }
-};
+	    handle_request : function (connection, msg, callback){
+	        try{
+	            var res = {};
+	            var localMsg = JSON.stringify(msg);
+	            var coll = connection.mongoConn.collection('property');
+	            coll.insert(msg, function(err, prop){
+	                if(err){
+	                    tool.logError(err);
+	                    res.code ="400";
+	                    callback(null, res);
+	                }
+	                else
+	                {
+	                	if(msg.is_auction == "Y"){
+	                	var options = {host_min_amt:localMsg.price, max_bid_price: localMsg.price, property_name: localMsg.description,property_id:prop.insertedIds};
+	                	mysql.execute_query(function (err,result) {
+	                         if(err){
+	                        	 tool.logError(err);
+	                             res.code ="400";
+	                             callback(null, res);
+	                         }
+	                         else {
+                                 res.propertyId = prop.insertedIds;
+                                 res.code ="200";
+                                 callback(null, res);
+	                             }
+	                     },sql_queries.INSERT_PRODUCT_IN_BIDDING,options);
+	                	}
+	                	 else
+	                     {
+	                		 res.propertyId = prop.insertedIds;
+	                         res.code ="200";
+	                         callback(null, res);
+	                     }
+	                }
+	            });
+	        }
+	        catch(err){
+	            tool.logError(err);
+	            res.statusCode = "500";
+	            callback(null, res);
+	        }
+	    }
+	};
 
 var searchProperty = {
     handle_request : function (connection,msg,callback) {
