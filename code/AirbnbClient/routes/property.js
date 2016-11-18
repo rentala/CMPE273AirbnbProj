feba14c735f50736a851c9c6f0f43ea753bc7a1c
rentala <anudeep.rentala@gmail.com>
2016-11-17 22:23:03 -0800

    file uplaod

--- code/AirbnbClient/routes/property.js
+++ code/AirbnbClient/routes/property.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var mq_client = require('../rpc/client');
+var multer = require('multer');
 
 router.get('/search',function (req,res,next) {
     var city = req.param("city");
@@ -27,27 +28,44 @@ router.get('/search',function (req,res,next) {
 
 });
 
+router.post('/list', function (req, res, next)  {
 
-
-
-router.post('/listProperty', function (req, res, next)  {
-    var json_responses;
-
-    var msg_payload = req.body;
-
-    mq_client.make_request('list_property_queue', msg_payload, function(err,results){
-        if(err){
-            json_responses = {
-                "failed" : "failed"
-            };
-        } else {
-            json_responses = {
-                "propertyId" : results.insertedIds[0]
-            };
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, '../AirbnbClient/public/uploads');
+        },
+        filename: function (req, file, cb) {
+            var datetimestamp = Date.now();
+            imagePath = file.fieldname + '-'
+                + datetimestamp + '.'
+                + file.originalname.split('.')[file.originalname.split('.').length -1];
+            cb(null, imagePath);
         }
-        res.statusCode = results.code;
-        res.send(json_responses);
     });
+    var upload = multer({ storage: storage}).array('photos');
+    var json_responses;
+    upload(req,res,function(err) {
+        if (err) {
+            res.json({error_code: 1, err_desc: err});
+            return;
+        }
+        var msg_payload = req.body;
+
+        mq_client.make_request('list_property_queue', msg_payload, function(err,results){
+            if(err){
+                json_responses = {
+                    "failed" : "failed"
+                };
+            } else {
+                json_responses = {
+                    "propertyId" : results.insertedIds[0]
+                };
+            }
+            res.statusCode = results.code;
+            res.send(json_responses);
+        });
+    });
+
 });
 
 router.get('/test', function (req, res, next)  {
