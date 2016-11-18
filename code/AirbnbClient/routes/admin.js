ee902ba20d60d4134f61995696189e85deb695bd
Smitha Muthuvenkataramani <smitha.muthuvenkataramani@sjsu.edu>
2016-11-17 16:45:06 -0800

    admin commit

--- code/AirbnbClient/routes/admin.js
+++ code/AirbnbClient/routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mq_client = require('../rpc/client');
 var passport = require('passport');
 
-//POST method to approve host...making changes
+//POST method to approve host...making changes in this admin file
 router.post('/approveHost', function (req, res)  {
     
 	console.log("Request Data  : " + JSON.stringify(req.body));
