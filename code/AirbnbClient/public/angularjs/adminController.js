e22eaa982f66037c43978146c21cbae08254e246
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-11-18 01:36:27 -0800

    no message

---
+++ code/AirbnbClient/public/angularjs/adminController.js
@@ -0,0 +1,26 @@
+
+app.controller('adminController',function($scope,$http,$state,$rootScope){
+
+    $scope.logIn = function(){
+        $http({
+            method:"POST",
+            url:"/api/admin/adminCheckLogin",
+            data:{
+                "email":$scope.emailAddress,
+                "password":$scope.password
+            }
+        }).success(function(data){
+            if(data.statusCode=="200"){
+                $('.modal-backdrop').remove();
+                //console.log("Login successful");
+                $state.go('home');
+            }
+            else if(data.statusCode=="400"){
+                $scope.loginError="Wrong email address or password";
+            }
+        })
+    }
+});
+/**
+ * Created by krishna.r.k on 11/18/2016.
+ */
