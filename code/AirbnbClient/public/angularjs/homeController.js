8a42e0bc94bcb4ab0a1aba8ad90d68a03971f032
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-11-17 17:38:42 -0800

    Added Search result

--- code/AirbnbClient/public/angularjs/homeController.js
+++ code/AirbnbClient/public/angularjs/homeController.js
@@ -1,4 +1,26 @@
 
 app.controller('homeController',function($scope,$http,$state,$rootScope){
+    $scope.searchProperty=function(){
+        $http({
+            "method":"POST",
+            "url":"/search",
+            "data":{
+                "city":$scope.city,
+                "start_date":$scope.start_date,
+                "end_date":$scope.end_date,
+                "guests":$scope.guests
 
+            }
+        }).success(function(data){
+            if(data.status_code=="200"){
+                $scope.valid_property=data.valid_property;
+            }
+            else if(data.status_code=="401"){
+                $scope.errMsg=data.errMsg;
+            }
+            else if(data.status_code=="402"){
+                $scope.errMsg=data.errMsg;
+            }
+        })
+    }
 });
--- code/AirbnbClient/public/angularjs/route_config.js
+++ code/AirbnbClient/public/angularjs/route_config.js
@@ -29,6 +29,18 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
         controller : 'homeController'
     });
 
+    $stateProvider.state('propertySearch', {
+        url : '/home',
+        views : {
+            'header' : {
+                templateUrl : 'templates/home/searchResult_header.html',
+            },
+            'content' : {
+                templateUrl : 'templates/home/searchResult.html',
+            }
+        },
+        controller : 'propertySearchController'
+    });
     /*$stateProvider.state('msg_disp', {
         url : '/msgDisp/:msg',
         views : {
