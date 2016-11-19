a9c64d73200b4bca6f87bc5df00c547a44112af5
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-11-18 16:53:13 -0800

    Merge branches 'master' and 'master' of https://github.com/varsha-kankariya/CMPE273AirbnbProj

--- code/AirbnbClient/public/angularjs/homeController.js
+++ code/AirbnbClient/public/angularjs/homeController.js
@@ -1,4 +1,14 @@
 
 app.controller('homeController',function($scope,$http,$state,$rootScope){
+    $scope.becomeHostPage = function(){
+        $http({
+            "method":"POST",
+            "url":"/host",
 
+        }).success(function(data){
+
+        })
+    }
+
+    $scope.
 });
