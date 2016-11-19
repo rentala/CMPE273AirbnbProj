660434484bcb4461892fc960f4498e07ed65754d
Tarshith-Vishnu-Teja <vishnuteja.pernapati@sjsu.edu>
2016-11-18 17:40:57 -0800

    dev 2 -ghtesting 2

--- code/RabbitMQServer/db/mysql.js
+++ code/RabbitMQServer/db/mysql.js
@@ -8,7 +8,7 @@ function getPool(connLimit) {
         connectionLimit : connLimit,
         host            : 'localhost',
         user            : 'root',
-        password        : '2993',
+        password        : '2994',
         database        : 'airbnb',
         multipleStatements : true
     });
