35ca6501e6c2eb597ba0485022248002f142b78c
VarshaKankariya <varsha.kankariya@sjsu.edu>
2016-11-20 11:31:44 -0800

    Admin analysis : Updated following queries : FETCH_CITY_WISE_DATA, FETCH_TOP_HOST, FETCH_TOP_PROP

--- code/RabbitMQServer/db/sql_queries.js
+++ code/RabbitMQServer/db/sql_queries.js
@@ -2,9 +2,9 @@
 var constants  =  require('node-constants');
 
 constants.define(exports, {
-    FETCH_CITY_WISE_DATA: "select trip.property_id,trip.property_name,sum(trip.trip_price),extract(year from trip.trip_approved_time) from airbnb.trip where trip.property_id IN (?) and trip.trip_status='ACCEPTED'group by trip.property_id, extract(year from trip.trip_approved_time) order by extract(year from trip.trip_approved_time);",
-    FETCH_TOP_HOST:"select trip.user_id, sum(trip.trip_price) as revenue,extract(month from trip.trip_approved_time) as trip_month from airbnb.trip where trip.trip_status='ACCEPTED' and month(trip_approved_time)= (month(now())-1) group by trip.user_id order by revenue desc;",
-    FETCH_TOP_PROP: "select trip.property_name , sum(trip.trip_price) as property_revenue from airbnb.trip where trip.trip_status ='ACCEPTED' group by trip.property_id order by property_revenue",
+    FETCH_CITY_WISE_DATA: "SELECT SUM(trip.trip_price) as total_revenue,YEAR(date(trip.trip_approved_time)) as trip_year FROM trip WHERE trip.property_id IN (?) and trip.trip_status = 'ACCEPTED' GROUP BY trip.property_id , trip_year ORDER BY trip_year limit ?;",
+    FETCH_TOP_HOST:"SELECT trip.host_id as host_id, sum(trip.trip_price) AS total_revenue FROM trip WHERE trip.trip_status = 'ACCEPTED' and month(date(trip.trip_approved_time)) = month(now()-interval 1 month) GROUP BY host_id ORDER BY total_revenue DESC LIMIT ?;",
+    FETCH_TOP_PROP: "SELECT trip.property_id AS property_id,year(date(trip.trip_approved_time)) as trip_year,SUM(trip.trip_price) AS property_revenue FROM airbnb.trip WHERE trip.trip_status = 'ACCEPTED' GROUP BY trip.property_id , trip_year ORDER BY property_revenue DESC LIMIT ?;",
     FETCH_TRIP_DATES : "SELECT trip.property_id,trip.checkin_date,trip.checkout_date from airbnb.trip where property_id IN (?) and user_id!=?;",
     FETCH_USER_TRIP_DETAILS : "SELECT trip.trip_id as trip_id,trip.user_id as user_id,trip.property_id as property_id,trip.host_id as host_id,trip.billing_id as billing_id,trip.checkin_date as checkin_date,trip.checkout_date as checkout_date,trip.no_of_guests as no_of_guests,trip.trip_status as trip_status from airbnb.trip where trip.user_id=?;",
     FETCH_HOST_TRIP_DETAILS : "SELECT trip.trip_id as trip_id,trip.user_id as user_id,trip.property_id as property_id,trip.host_id as host_id,trip.billing_id as billing_id,trip.checkin_date as checkin_date,trip.checkout_date as checkout_date,trip.no_of_guests as no_of_guests,trip.trip_status as trip_status from airbnb.trip where trip.host_id=?;",
