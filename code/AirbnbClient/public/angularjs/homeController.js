33156124aae352fc357baddec38c18990f1c862e
jpranjal <jpranjal1304@gmail.com>
2016-11-21 17:38:25 -0800

    updated parameters

--- code/AirbnbClient/public/angularjs/homeController.js
+++ code/AirbnbClient/public/angularjs/homeController.js
@@ -19,7 +19,24 @@ app.controller('homeController',function($scope,$http,$state,$rootScope){
     $scope.search=function(){
         $http({
             "method":"POST",
-            "url":"/api/property/search"
+            "url":"/api/property/search",
+            data : {
+				"city" : $scope.whereTo,
+				"guests" : $scope.guests,
+				"start_date": $scope.start_date,
+				"end_date": $scope.end_date
+			}
+        }).success(function(data){
+            if(data.status_code=="200"){
+                //$('.modal-backdrop').remove();
+               // $rootScope.user_dtls = JSON.parse(data.user);
+                alert("inside");
+                //console.log("Login successful");
+                window.location.assign("/api/auth/home");
+            }
+            else if(data.status_code=="400"){
+                $scope.loginError="Wrong email address or password";
+            }
         })
     }
 });
