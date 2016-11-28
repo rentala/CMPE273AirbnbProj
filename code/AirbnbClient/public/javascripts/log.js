36028ab6eb58e383eb71b82c5b772cec7fbd8daa
rentala <anudeep.rentala@gmail.com>
2016-11-28 12:28:09 -0800

    logging cases added

--- code/AirbnbClient/public/javascripts/log.js
+++ code/AirbnbClient/public/javascripts/log.js
@@ -6,20 +6,26 @@ var logEvent = function (e, text) {
     $.ajax({
         method: "POST",
         url: "/log",
-        data: { event: e, text: text }
+        data: { event: e, text: text, type: type }
     }).done(function(res) {
         console.log(res)
     });
 
 }
+
 $(function () {
+    var type;
     $("body").on("click", "a" , function(e) {
-        logEvent(e.target.innerText + " link", "Id: " + e.target.id +" link clicked "+ e.target.href)
+        type = e.target.href.indexOf('api/property/id') != -1 ? "PROPERTYCLICK": "PAGECLICK";
+        logEvent(e.target.innerText + " link", "Id: " + e.target.id +" link clicked "+ e.target.href, type)
     });
     $("body").on("click", "button" , function(e) {
         logEvent(e.target.innerText + " button", "Id: " + e.target.id +" form action "+ e.target.formAction + " form method" +e.target.formMethod )
     });
-    $("body").on("click", "input" , function(e) {
+    $("body").on("click", "input[type='button']" , function(e) {
+        logEvent(e.target.innerText + " button", "Id: " + e.target.id +" form action "+ e.target.formAction + " form method" +e.target.formMethod )
+    });
+    $("body").on("click", "input[type='submit']" , function(e) {
         logEvent(e.target.innerText + " button", "Id: " + e.target.id +" form action "+ e.target.formAction + " form method" +e.target.formMethod )
     });
 })
