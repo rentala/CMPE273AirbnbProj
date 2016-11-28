87d7558692771db5612adb5a92e135388eb204c2
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-11-28 01:03:57 -0800

    no message

--- code/RabbitMQServer/services/analytics.js
+++ code/RabbitMQServer/services/analytics.js
@@ -3,10 +3,14 @@ var sql_queries = require('../db/sql_queries');
 var mysql = require('../db/mysql');
 var property = require('./property');
 var ObjectID = require('mongodb').ObjectId;
+var d3 = require('d3');
+
 
 var topProp = {
     handle_request: function (connection,msg,callback) {
         var res={};
+        var no_of_props = parseInt(msg.no_of_props);
+        console.log("No of properties : " + no_of_props);
         mysql.execute_query(function (err,result) {
             if(err){
                 res = {"statusCode":400};
@@ -15,15 +19,20 @@ var topProp = {
             }
             else {
                 if(result.length>0){
-                    res = {"statusCode":200,"top_property":result};
-                    callback(null, res);
+               	
+               	 var top_prop_data = d3.nest()
+               	  .key(function(d) { return d.trip_year; })
+               	  .entries(result);
+               	 
+                   res = {"statusCode":200,"top_property":top_prop_data};
+                   callback(null, res);
                 }
                 else{
                     res = {"statusCode":401};
                     callback(null,res);
                 }
             }
-        },sql_queries.FETCH_TOP_PROP,msg.no_of_props);
+        },sql_queries.FETCH_TOP_PROP,no_of_props);
     }
 };
 
@@ -34,7 +43,7 @@ var cityWiseData = {
 
       try{
           var coll=connection.mongoConn.collection('property');
-          coll.find({"city":msg.city}).toArray(function (err, records) {
+          coll.find({"address.city":msg.city},{'_id':1}).toArray(function (err, records) {
               if(err){
                   res = {"statusCode":400};
                   tool.logError(err);
@@ -42,8 +51,8 @@ var cityWiseData = {
               }
               else {
                   if(records.length>0){
-                      city_wise_property = property.getPropertyArray(records);
-                      console.log(city_wise_property);
+                      city_wise_property = getPropertyIds(records);
+                     // console.log("Property_id : " + JSON.stringify(city_wise_property));
                       mysql.execute_query(function (err,result) {
                           if(err){
                               res = {"statusCode":400};
@@ -69,6 +78,16 @@ var cityWiseData = {
       }
   }
 };
+
+
+function getPropertyIds(records){
+	var data = [];
+	for(record in records){
+		console.log("Record : " +JSON.stringify(record));
+		data.push(record);
+	}
+	return data;
+}
 
 var topHost = {
     handle_request: function (connection,msg,callback) {
