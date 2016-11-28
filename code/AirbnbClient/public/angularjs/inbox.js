6a8209875d89b9914f1a2418d083f90340e3d41d
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-11-28 01:02:46 -0800

    inbox page

--- code/AirbnbClient/public/angularjs/inbox.js
+++ code/AirbnbClient/public/angularjs/inbox.js
@@ -1,17 +1,17 @@
 var app = angular.module('myInbox',[]);
 app.controller('myInboxController',function($scope,$http){
+    $http({
+        method: "POST",
+        url: "/api/inbox/inboxContent"
+    }).success(function (data) {
+    if (data.status_code == "200") {
+        $scope.details = data.result;
+    }
+     else {
+        console.log("vhhvnv");
+    }
+    })
+    $scope.acceptTrip = function(){
 
-    $scope.some = function() {
-        $http({
-            method: "POST",
-            url: "/api/inbox/inboxContent"
-        }).success(function (data) {
-            if (data.status_code == "200") {
-                $scope.details = data.result;
-            }
-            else {
-                console.log("vhhvnv");
-            }
-        })
     }
 })
