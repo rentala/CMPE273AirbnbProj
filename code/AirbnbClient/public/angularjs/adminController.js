312828c90df377d7a68ac3ed0b583f52e7d26bc2
VarshaKankariya <varsha.kankariya@sjsu.edu>
2016-11-29 00:52:50 -0800

    Updated code for adding charts on admin dashboard.

--- code/AirbnbClient/public/angularjs/adminController.js
+++ code/AirbnbClient/public/angularjs/adminController.js
@@ -219,18 +219,32 @@ $scope.getHostsByCity = function(){
 //***************BILLS End here*****************
 
     $scope.topProperties = function(){
+   	 
+   	 console.log("Top Properties");
+   	 
         $scope.showTopProperties = true;
         $scope.showCityWiseRevenues = false;
+        //Delete the 'svg' tag if it exists
+        var svgCityWiseRevenueDiv = d3.select('#cityWiseRevenueDiv').select("svg");
+        if(svgCityWiseRevenueDiv){ svgCityWiseRevenueDiv.remove();}
+        var svgTopHostDiv = d3.select('#topHostDiv').select("svg");
+        if(svgTopHostDiv){ svgTopHostDiv.remove();}
+        
         $scope.showTopHost = false;
         $http({
-            method : "GET",
+            method : "POST",
             url : "/api/analytics/topProp",
             data :{
-                "no_of_props" : 10
+                "no_of_props" : 10,
+                "year" : "2016"
             }
         }).success(function(data){
+      	  console.log("Top Properites controller : " + JSON.stringify(data));
             if(data.status_code == 200){
                 $scope.topProperties = data.top_property;
+                console.log("Angular Top Properties : " + JSON.stringify($scope.topProperties) );
+                var svg = d3.select('#topPropertiesDiv') .append('svg') .attr('height', 200) .attr('width', 300);
+                display2DGraph( $scope.topProperties,svg)
             }
             else if(data.status_code == 400){
                 console.log("error on service side");
@@ -242,21 +256,31 @@ $scope.getHostsByCity = function(){
     }
 
     $scope.cityWiseRevenue = function(){
+   	 
+   	 console.log("City Wise Revenue");
         $scope.showTopProperties = false;
         $scope.showCityWiseRevenues = true;
         $scope.showTopHost = false;
+        
+        var svgTopPropertiesDiv = d3.select('#topPropertiesDiv').select("svg");
+        if(svgTopPropertiesDiv){ svgTopPropertiesDiv.remove();}
+        var svgTopHostDiv = d3.select('#topHostDiv').select("svg");
+        if(svgTopHostDiv){ svgTopHostDiv.remove();}
 
         $scope.getCityRevenue = function(){
             //console.log($scope.cityForRevenue);
             $http({
-                method : "GET",
+                method : "POST",
                 url : "/api/analytics/cityWiseData",
                 data : {
-                    "city" : $scope.cityForRevenue
+                    //"city" : $scope.cityForRevenue
+                    "city" : "San Jose"
                 }
             }).success(function(data){
                 if(data.status_code == 200){
                     $scope.cityWiseRevenue = data.city_wise_data;
+                    var svg = d3.select('#cityWiseRevenueDiv') .append('svg') .attr('height', 200) .attr('width', 300);
+                    display2DGraph( $scope.cityWiseRevenue,svg)
                 }
                 else if(data.status_code == 400){
                     console.log("error on service");
@@ -269,18 +293,30 @@ $scope.getHostsByCity = function(){
     } 
 
     $scope.topHost = function(){
+   	 
+   	 console.log("Top Host");
         $scope.showTopProperties = false;
         $scope.showCityWiseRevenues = false;
         $scope.showTopHost = true;
+        
+        
+        var svgTopPropertiesDiv = d3.select('#topPropertiesDiv').select("svg");
+        if(svgTopPropertiesDiv){ svgTopPropertiesDiv.remove();}
+        
+        var svgCityWiseRevenueDiv = d3.select('#cityWiseRevenueDiv').select("svg");
+        if(svgCityWiseRevenueDiv){ svgCityWiseRevenueDiv.remove();}
+        
         $http({
-            method : "GET",
+            method : "POST",
             url : "/api/analytics/topHost",
             data : {
                 "no_of_hosts" : 2
             }
         }).success(function(data){
             if(data.status_code == 200){
-                $scope.topHost = data.data;
+                $scope.topHost = data.top_host;
+                var svg = d3.select('#topHostDiv') .append('svg') .attr('height', 200) .attr('width', 300);
+                display2DGraph( $scope.topHost,svg)
             }
             else if(data.status_code == 400){
                 console.log("error on service");
@@ -290,5 +326,56 @@ $scope.getHostsByCity = function(){
             }
         })
     }
+    
+    
+    var display2DGraph = function(data,svg){
+ 		
+ 		//var data =$scope.dataForPropClicks;
+ 		
+ 		
+ 		//var svg = d3.select('#propertyClicksDiv') .append('svg') .attr('height', 200) .attr('width', 300);
+ 		var margin = {top: 20, right: 20, bottom: 30, left: 40},
+ 		 width = +svg.attr("width") - margin.left - margin.right,
+ 	    height = +svg.attr("height") - margin.top - margin.bottom;
+ 		
+ 		/*var svg = d3.select("svg"),
+ 	    margin = {top: 20, right: 20, bottom: 30, left: 40},
+ 	    width = +svg.attr("width") - margin.left - margin.right,
+ 	    height = +svg.attr("height") - margin.top - margin.bottom;*/
+
+ 		var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+   	    y = d3.scaleLinear().rangeRound([height, 0]);
+
+ 		var g = svg.append("g")
+   	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+ 		x.domain(data.map(function(d) { return d.key; }));
+ 		  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+ 		  g.append("g")
+ 		      .attr("class", "axis axis--x")
+ 		      .attr("transform", "translate(0," + height + ")")
+ 		      .call(d3.axisBottom(x));
+
+ 		  g.append("g")
+ 		      .attr("class", "axis axis--y")
+ 		      .call(d3.axisLeft(y).ticks(10))
+ 		    .append("text")
+ 		      .attr("transform", "rotate(-90)")
+ 		      .attr("y", 6)
+ 		      .attr("dy", "0.71em")
+ 		      .attr("text-anchor", "end")
+ 		      .text("Clicks");
+
+ 		  g.selectAll(".bar")
+ 		    .data(data)
+ 		    .enter().append("rect")
+ 		      .attr("class", "bar")
+ 		      .attr("x", function(d) { return x(d.key); })
+ 		      .attr("y", function(d) { return y(d.value); })	
+ 		      .attr("width", x.bandwidth())
+ 		      .attr("height", function(d) { return height - y(d.value); });
+ 		
+ 	}; 
 })
 
