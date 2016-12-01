d4cf534078ddaf8479a699d62fe2212655391279
KrishnaRattihalliKantharaj <krishna.rattihallikantharaj@sjsu.edu>
2016-12-01 15:38:50 -0800

    modified user profile

--- code/AirbnbClient/public/angularjs/userProfileController.js
+++ code/AirbnbClient/public/angularjs/userProfileController.js
@@ -1,7 +1,13 @@
 /**
  * New node file
  */
-var hostProfileApp = angular.module('hostProfileApp', []);
-hostProfileApp.controller('hostProfileController', function($scope, $http, $rootScope){
-	
-}) 
+var app = angular.module('hostProfileApp',[]);
+app.controller('hostProfileController',function($scope,$http){
+
+    $http({
+        method:"POST",
+        url:"/api/profile/getUserDetailsForProfile"
+    }).success(function(data){
+
+    })
+})
