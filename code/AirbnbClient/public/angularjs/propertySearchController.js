e89eac1153e860d25a72b40f406e05f416e7a9c9
rentala <anudeep.rentala@gmail.com>
2016-12-04 12:27:35 -0800

    date validation added

--- code/AirbnbClient/public/angularjs/propertySearchController.js
+++ code/AirbnbClient/public/angularjs/propertySearchController.js
@@ -43,48 +43,73 @@ app.config(['$locationProvider', function($locationProvider) {
 		        })
 				}
 			}
-			
-			$scope.editTrip = function(price){
+			$scope.hasErrors = false;
+			$scope.checkDateValidity = function () {
+				if($scope.check_in < $scope.check_out)
+				{
+					$scope.hasErrors = false;
+				}
+				else {
+					$scope.hasErrors = true;
+				}
+
+			}
+			$scope.minDate = new Date();
+			$scope.minDate = $scope.minDate > new Date(propertyDates.start_date) ? new Date(propertyDates.start_date) : $scope.minDate;
+			$scope.MaxCheckOut = new Date(propertyDates.end_date);
+			/*var mon = $scope.minDate.getMonth() + 1;
+			$scope.minDate = $scope.minDate.getFullYear() + "-"
+				+ mon + "-" +
+				$scope.minDate.getDate();
+				*/
+			$scope.editTrip = function(){
 				var property_id = document.getElementById("property_id2").defaultValue;
 				var trip_id = document.getElementById("trip_id2").defaultValue;
 				var start_date = $scope.check_in;
 				var end_date = $scope.check_out;
 				var guests = document.getElementById("guests2").defaultValue;
-				if(start_date == undefined || end_date ==undefined){
-					alert("Enter checkin and checkout dates");
+				if($scope.hasErrors){
+					alert("Enter Valid Dates")
 				}
 				else{
-				$http({
-		            method:"POST",
-		            url:"/api/trip/editTrip",
-		            data:{
-		            	property_id:property_id,
-		            	trip_id:trip_id,
-		            	guests:guests,
-		            	start_date:start_date,
-		            	end_date:end_date
-		            }
-		        }).success(function(data){
-		        	if(data.status_code == "200"){
-		        		var diff = eval(data.newTripPrice-parseInt(price));
-		        		if(diff>0){
-		        			alert("you will have to pay more");
-		        			window.location.assign("/api/property/paymentGateway/e/"+diff);
-		        		}	
-	        			else	
-	        				alert("Trip updated. No extra charges")
-		        	}
-		        	else{
-		        		alert("Sorry cannot update trip for following dates");
-		        	}
-		        })
+					if(start_date == undefined || end_date ==undefined){
+						alert("Enter checkin and checkout dates");
+
+					}
+					else{
+						$http({
+							method:"POST",
+							url:"/api/trip/editTrip",
+							data:{
+								property_id:property_id,
+								trip_id:trip_id,
+								guests:guests,
+								start_date:start_date,
+								end_date:end_date
+							}
+						}).success(function(data){
+							if(data.status_code == "200"){
+								var diff = eval(data.newTripPrice-parseInt($scope.tripPrice()));
+								if(diff>0){
+									alert("you will have to pay more");
+									window.location.assign("/api/property/paymentGateway/e/"+diff);
+								}
+								else
+									alert("Trip updated. No extra charges")
+							}
+							else{
+								alert("Sorry cannot update trip for following dates");
+							}
+						})
+					}
 				}
+
 			}
 			$scope.hostPage = function(){
 				window.location.assign("/host");
 			}
-
-
+			$scope.check_in = new Date();
+			$scope.check_out = new Date();
 			var queries = $location.search();
 			var trip_id = queries.tip;
 			$http({
