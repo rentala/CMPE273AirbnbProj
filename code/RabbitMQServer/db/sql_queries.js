58b681bdff768a193a97444e8ad5972e878645c6
jpranjal <jpranjal1304@gmail.com>
2016-12-03 19:23:59 -0800

    changed date format in trip table get queries

--- code/RabbitMQServer/db/sql_queries.js
+++ code/RabbitMQServer/db/sql_queries.js
@@ -3,8 +3,8 @@ var constants  =  require('node-constants');
 
 constants.define(exports, {
     FETCH_BID_INFO:"select bidding_dtl.bidder_id,bidding_dtl.bid_price,bidding_dtl.bid_time,bidding_dtl.property_name from airbnb.bidding_dtl where property_id=?;",
-    FETCH_BILLING_DTLS:"SELECT trip.user_id,trip.property_id,trip.property_name,billing.billing_id,trip.trip_price,trip.checkin_date,trip.checkout_date,trip.no_of_guests FROM airbnb.trip ,airbnb.billing WHERE trip.trip_id = ? and trip.trip_id = billing.trip_id and billing.bill_status = 'CREATED';",
-    FETCH_ALL_BILLS : "SELECT trip.user_id,trip.property_id,trip.property_name,billing.billing_id,trip.trip_price,trip.checkin_date,trip.checkout_date,trip.no_of_guests,year(date(trip.trip_approved_time)) as year,month(date(trip.trip_approved_time)) as month,trip.trip_approved_time FROM airbnb.trip ,airbnb.billing WHERE billing.bill_status = 'CREATED';",
+    FETCH_BILLING_DTLS:"SELECT trip.user_id,trip.property_id,trip.property_name,billing.billing_id,trip.trip_price,DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date,DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date,trip.no_of_guests FROM airbnb.trip ,airbnb.billing WHERE trip.trip_id = ? and trip.trip_id = billing.trip_id and billing.bill_status = 'CREATED';",
+    FETCH_ALL_BILLS : "SELECT trip.user_id,trip.property_id,trip.property_name,billing.billing_id,trip.trip_price,DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date,DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date,trip.no_of_guests,year(date(trip.trip_approved_time)) as year,month(date(trip.trip_approved_time)) as month,trip.trip_approved_time FROM airbnb.trip ,airbnb.billing WHERE billing.bill_status = 'CREATED';",
     FETCH_CITY_WISE_DATA: "SELECT YEAR(DATE(trip.trip_approved_time)) AS trip_year, SUM(trip.trip_price) AS total_revenue FROM trip WHERE trip.property_id IN (?) AND trip.trip_status = 'ACCEPTED' GROUP BY trip_year ORDER BY trip_year;",
     FETCH_TOP_HOST:"SELECT host_id, host_name, sum(trip.trip_price) AS total_revenue FROM trip WHERE trip.trip_status = 'ACCEPTED' and month(date(trip.trip_approved_time)) = month(now()-interval 1 month) GROUP BY host_id, host_name ORDER BY total_revenue DESC LIMIT ?;",
     FETCH_TOP_PROP: "SELECT property_name as property_name,SUM(trip.trip_price) AS revenue FROM airbnb.trip WHERE trip.trip_status = 'ACCEPTED' AND YEAR(DATE(trip.trip_approved_time)) = ? GROUP BY property_id , property_name ORDER BY revenue DESC LIMIT ?;",
@@ -15,20 +15,20 @@ constants.define(exports, {
     " then 'COMPLETED' "+
     " else trip.trip_status "+
     " end as trip_status, "+
-    " trip.trip_id, trip.user_id, trip.property_id, trip.host_id, trip.checkin_date, trip.checkout_date, trip.no_of_guests, trip.trip_approved_time, "+
+    " trip.trip_id, trip.user_id, trip.property_id, trip.host_id, DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date, DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date, trip.no_of_guests, trip.trip_approved_time, "+
     " trip.property_name, trip.trip_price, trip.host_name, trip.is_reviewed, trip.rating, trip.review_comment, trip.guest_name, "+
     "  (select b.billing_id from airbnb.billing b where b.trip_id = trip.trip_id) as billing_id  "+
     " from airbnb.trip trip  where trip.user_id=?; ",
-    FETCH_HOST_TRIP_DETAILS : "SELECT trip.trip_id as trip_id,trip.user_id as user_id,trip.property_id as property_id,trip.host_id as host_id,trip.checkin_date as checkin_date,trip.checkout_date as checkout_date,trip.no_of_guests as no_of_guests,trip.trip_status as trip_status,  (select b.billing_id from airbnb.billing b where b.trip_id = trip.trip_id) as billing_id from airbnb.trip where trip.host_id=?;",
+    FETCH_HOST_TRIP_DETAILS : "SELECT trip.trip_id as trip_id,trip.user_id as user_id,trip.property_id as property_id,trip.host_id as host_id,DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date,DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date,trip.no_of_guests as no_of_guests,trip.trip_status as trip_status,  (select b.billing_id from airbnb.billing b where b.trip_id = trip.trip_id) as billing_id from airbnb.trip where trip.host_id=?;",
     DELETE_TRIP : "delete from airbnb.trip where trip.trip_id=?;",
     DELETE_BILL : "update airbnb.billing set bill_status='DELETED' where airbnb.billing.billing_id=?; ",
     CREATE_BILL : "insert into airbnb.billing(billing_id,trip_id,bill_status) values(null,?,?);" ,
-    USER_COMPLETED_TRIPS : "select trip_id, property_id, property_name, host_id, host_name, checkin_date, checkout_date, no_of_guests from trip where user_id = ? and date(checkout_date) < curdate();",
+    USER_COMPLETED_TRIPS : "select trip_id, property_id, property_name, host_id, host_name, DATE_FORMAT(checkin_date, '%Y-%m-%d') as checkin_date, DATE_FORMAT(checkout_date, '%Y-%m-%d') as checkout_date, no_of_guests from trip where user_id = ? and date(checkout_date) < curdate();",
     CREATE_TRIP : "INSERT into airbnb.trip (user_id, property_id, property_name, host_id, checkin_date, checkout_date, no_of_guests, trip_status, trip_price, guest_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?);",
     INSERT_PRODUCT_IN_BIDDING : "insert into airbnb.bidding set ? ",
     UPDATE_TRIP : "update airbnb.trip set trip_status = ?, trip_approved_time = CURRENT_TIMESTAMP where trip.trip_id=?",
     UPDATE_TRIP_DATES : "update airbnb.trip set trip_status = 'PENDING', checkin_date=?, checkout_date=?, no_of_guests= ?, trip_price=? where trip.trip_id=?",
-    FETCH_PENDING_TRIPS: "select trip.user_id,trip.property_id,trip.property_name,trip.checkin_date,trip.checkout_date,trip.no_of_guests,trip.trip_price,trip.trip_status from airbnb.trip where trip.host_id=? and trip.trip_status='Pending';",
+    FETCH_PENDING_TRIPS: "select trip.user_id,trip.property_id,trip.property_name,DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date,DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date,trip.no_of_guests,trip.trip_price,trip.trip_status from airbnb.trip where trip.host_id=? and trip.trip_status='Pending';",
     INSERT_BID : "Insert into airbnb.bidding_dtl (bid_id, bidder_id, bid_price, property_id, property_name, bidder_name) values ( ?,?,?,?,?,?)",
     INBOX : "select * from airbnb.trip where host_id = ? and trip_status = 'PENDING';",
     FETCH_MAX_BID:"SELECT max_bid_price,host_min_amt FROM airbnb.bidding  WHERE property_id=?;",
@@ -44,7 +44,7 @@ constants.define(exports, {
     " then 'COMPLETED' "+
     " else trip.trip_status "+
     " end as trip_status, "+
-    " trip.trip_id, trip.host_reviewed, trip.user_id, trip.property_id, trip.host_id, trip.checkin_date, trip.checkout_date, trip.no_of_guests, trip.trip_approved_time, "+
+    " trip.trip_id, trip.host_reviewed, trip.user_id, trip.property_id, trip.host_id, DATE_FORMAT(trip.checkin_date, '%Y-%m-%d') as checkin_date, DATE_FORMAT(trip.checkout_date, '%Y-%m-%d') as checkout_date, trip.no_of_guests, trip.trip_approved_time, "+
     " trip.property_name, trip.trip_price, trip.host_name, trip.is_reviewed, trip.rating, trip.review_comment, trip.guest_name, "+
     "  (select b.billing_id from airbnb.billing b where b.trip_id = trip.trip_id) as billing_id  "+
     " from airbnb.trip trip  where trip.host_id=? and trip.trip_status='ACCEPTED' ",
