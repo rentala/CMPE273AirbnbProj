fec4e2babc9c9214c12c7284511ec115897602a1
Tarshith-Vishnu-Teja <vishnuteja.pernapati@sjsu.edu>
2016-12-04 12:19:43 -0800

    testing

---
+++ code/AirbnbClient/routes/mongo.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Rentala on 02-11-2016.
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
+var connected = false; /**Connects to the MongoDB Database with the provided URL**/
+exports.connect = function(url, callback){
+    /*var options = {
+        db : {
+            numberOfRetries : 5
+        },
+        server : {
+            auto_reconnect : true,
+            poolSize : 20000,
+            socketOptions : {
+                connectTimeoutMS : 5000
+            }
+        },
+        replSet : {},
+        mongos : {}
+    };*/
+    MongoClient.connect(url, /*options,*/ function(err, _db){
+        if (err) {
+            throw new Error('Could not connect: '+err);
+        }
+        db = _db;
+        connected = true;
+        console.log(connected +" is connected?");
+        callback(db);
+    });
+}; /**Returns the collection on the selected database**/
+exports.collection = function(name){
+    if (!connected) {
+        throw new Error('Must connect to Mongo before calling "collection"');
+    }
+    return db.collection(name, {connectTimeoutMS: 3000});
+};
